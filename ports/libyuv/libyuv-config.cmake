bc9fc0c690d2c3b11f9fb4a98bb54ddcfeb284d2
Dmitry Chapyshev <dmitry@aspia.ru>
2022-06-05 00:01:21 +0500

    Remove jpeg dependency.

--- ports/libyuv/libyuv-config.cmake
+++ ports/libyuv/libyuv-config.cmake
@@ -1,5 +1,4 @@
 include(CMakeFindDependencyMacro)
-find_dependency(JPEG)
 
 set(libyuv_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../include")
 include("${CMAKE_CURRENT_LIST_DIR}/libyuv-targets.cmake")
