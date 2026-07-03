9eaf4904bb0511117b5d6ef55840e35f4844d01f
Dmitry Chapyshev <dmitrii@ninjarmm.com>
2026-07-03 15:53:01 +0500

    Set X_VCPKG_FORCE_VCPKG_X_LIBRARIES to static build X libraries.

--- triplets/x64-linux.cmake
+++ triplets/x64-linux.cmake
@@ -4,3 +4,8 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
+# Build the X11/xcb libraries from vcpkg as static libs instead of deferring to the system packages.
+# Without this the X ports (xcb, libx11, libxkbcommon, xcb-util-*, etc.) install empty packages and the
+# build links the system shared libraries; we want them statically linked into the host binaries.
+set(X_VCPKG_FORCE_VCPKG_X_LIBRARIES ON)
+
